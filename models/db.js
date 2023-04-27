e8406373fc52c0fd78a204a8a5a627211157f3b9
Joaogabriel148 <105577574+Joaogabriel148@users.noreply.github.com>
2023-04-27 13:54:48 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,14 +1,15 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('vitor','root','123456',   {
+const sequelize = new Sequelize('João','root','777777', {
     host: 'localhost',
     dialect: 'mysql'
 });
 
-     try {
-     sequelize.authenticate()
-        console.log('CONEXÃO COM O BANCO DE DADOS REALIZADA COM SUCESSO.');
-      } catch (error) {
+try {
+    sequelize.authenticate()
+    console.log('CONEXÃO COM O BANCO DE DADOS REALIZADA COM SUCESSO.');
+      } 
+catch (error) {
         console.error('ERRO CONEXÃO NÃO REALIZADA:', error)
       }
 
